103f165a15f7dcca0df999d4b23be1a8cb1c4a7b
Waqar Ahmed <waqarsonic1@gmail.com>
2018-07-17 08:10:48 +0500

    This commit adds ACME support for api v1

--- gui/system/forms.py
+++ gui/system/forms.py
@@ -2740,7 +2740,6 @@ class DNSAuthenticatorForm(MiddlewareModelForm, ModelForm):
         with client as c:
             schemas = c.call('dns.authenticator.schema_choices')
 
-        # FIXME: FOR SOME REASON ONCHANGE IS NOT BEING CALLED ON FORM CREATION - LOOK INTO THIS
         self.fields['authenticator'].choices = [
             (schema['name'], schema['title'])
             for schema in schemas
@@ -2754,7 +2753,7 @@ class DNSAuthenticatorForm(MiddlewareModelForm, ModelForm):
     def middleware_clean(self, data):
         data['attributes'] = json.loads(self.cleaned_data.get('attributes'))
         data.pop('credentials_schemas', None)
-        if self.instance:
+        if self.instance.id:
             data.pop('authenticator')
         return data
 
@@ -2766,7 +2765,6 @@ class CertificateACMEForm(MiddlewareModelForm, ModelForm):
     middleware_attr_schema = 'certificate'
     is_singletone = False
     middleware_job = True
-    complete_job = False
 
     cert_tos = forms.BooleanField(
         required=False,
@@ -2794,6 +2792,7 @@ class CertificateACMEForm(MiddlewareModelForm, ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.csr_id = kwargs.pop('csr_id')
+        self.complete_job = kwargs.pop('complete_job', False)
 
         super(CertificateACMEForm, self).__init__(*args, **kwargs)
 
@@ -2845,7 +2844,6 @@ class CertificateEditForm(MiddlewareModelForm, ModelForm):
     middleware_attr_schema = 'certificate'
     is_singletone = False
     middleware_job = True
-    complete_job = False
 
     cert_name = forms.CharField(
         label=models.Certificate._meta.get_field('cert_name').verbose_name,
